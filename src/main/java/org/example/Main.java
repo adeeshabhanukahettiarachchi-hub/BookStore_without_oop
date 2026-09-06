8f8e1a58825755219799186aae66d5b086b9717a
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-06 14:07:22 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Book data stored using separate lists
+        static ArrayList<String> bookTitles = new ArrayList<>();
+        static ArrayList<String> bookAuthors = new ArrayList<>();
+        static ArrayList<Double> bookPrices = new ArrayList<>();
+
+    }
+}
